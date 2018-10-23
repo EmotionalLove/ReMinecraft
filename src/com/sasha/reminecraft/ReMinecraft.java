a5902511ace85fc154444f1e4478b50705489e15
Sasha <sashwho@vivaldi.net>
2018-10-23 09:51:24 -0700

    Start sum good stuff

--- src/com/sasha/reminecraft/ReMinecraft.java
+++ src/com/sasha/reminecraft/ReMinecraft.java
@@ -5,10 +5,7 @@ import com.github.steveice10.packetlib.Client;
 import com.github.steveice10.packetlib.Server;
 import com.github.steveice10.packetlib.Session;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * The main Re:Minecraft class, where the majority of essential startup functions will be stored.
@@ -25,23 +22,31 @@ public class ReMinecraft {
      */
     public static final String VERSION = "1.0a";
 
+    public Logger logger = new Logger("RE:Minecraft " + VERSION);
     public Client minecraftClient = null;
     public Server minecraftServer = null;
     public MinecraftProtocol protocol;
     public List<ChildReClient> childClients = new ArrayList<>();
     public HashMap<Session, ChildReClient> sessionClients = new LinkedHashMap<>();
+
     /**
      * Launch Re:Minecraft and and setup the console command system.
      */
     public static void main(String[] args) {
-        // TODO
+        new ReMinecraft().start(args); // start Re:Minecraft before handling console commands
+        Scanner scanner = new Scanner(System.in);
+        String cmd = scanner.nextLine();
+        // TODO commands
     }
 
     /**
      * Launch (or relaunch) Re:Minecraft
      */
-    public void start() {
-        // TODO
+    public void start(String[] args) {
+        INSTANCE = this;
+        logger.log("// Starting RE:Minecraft " + VERSION + " \\\\");
+        // TODO register commands
+        
     }
 
 }

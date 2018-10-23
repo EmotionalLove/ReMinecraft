08ee9213cfd1806c59fd5e420a502a8edd91e7a3
Sasha <sashwho@vivaldi.net>
2018-10-23 09:25:19 -0700

    Initial commit

---
+++ src/com/sasha/reminecraft/ReMinecraft.java
@@ -0,0 +1,22 @@
+package com.sasha.reminecraft;
+
+/**
+ * The main ReMinecraft class, where the majority of essential startup functions will be stored.
+ */
+public class ReMinecraft {
+
+    /**
+     * Launch ReMinecraft and and setup the console command system.
+     */
+    public static void main(String[] args) {
+        // TODO
+    }
+
+    /**
+     * Launch (or relaunch) ReMinecraft
+     */
+    public void start() {
+        // TODO
+    }
+
+}

cf37062738ba9d8918690f22292d5606bbca05aa
Sasha <sashwho@vivaldi.net>
2018-10-23 09:43:22 -0700

    Create some variables.

---
+++ src/com/sasha/reminecraft/ChildReClient.java
@@ -0,0 +1,4 @@
+package com.sasha.reminecraft;
+
+public class ChildReClient {
+}
--- src/com/sasha/reminecraft/ReMinecraft.java
+++ src/com/sasha/reminecraft/ReMinecraft.java
@@ -1,19 +1,44 @@
 package com.sasha.reminecraft;
 
+import com.github.steveice10.mc.protocol.MinecraftProtocol;
+import com.github.steveice10.packetlib.Client;
+import com.github.steveice10.packetlib.Server;
+import com.github.steveice10.packetlib.Session;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 /**
- * The main ReMinecraft class, where the majority of essential startup functions will be stored.
+ * The main Re:Minecraft class, where the majority of essential startup functions will be stored.
  */
 public class ReMinecraft {
 
     /**
-     * Launch ReMinecraft and and setup the console command system.
+     * Singleton of this Re:Minecraft
+     */
+    public static ReMinecraft INSTANCE;
+
+    /**
+     * Current software version of Re:Minecraft
+     */
+    public static final String VERSION = "1.0a";
+
+    public Client minecraftClient = null;
+    public Server minecraftServer = null;
+    public MinecraftProtocol protocol;
+    public List<ChildReClient> childClients = new ArrayList<>();
+    public HashMap<Session, ChildReClient> sessionClients = new LinkedHashMap<>();
+    /**
+     * Launch Re:Minecraft and and setup the console command system.
      */
     public static void main(String[] args) {
         // TODO
     }
 
     /**
-     * Launch (or relaunch) ReMinecraft
+     * Launch (or relaunch) Re:Minecraft
      */
     public void start() {
         // TODO

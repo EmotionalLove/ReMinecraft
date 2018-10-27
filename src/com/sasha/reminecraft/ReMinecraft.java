ca5988884c852b11dc9fce5d68be3c8c8ded8478
Sasha <sashwho@vivaldi.net>
2018-10-27 07:09:12 -0700

    We should do this...

--- src/com/sasha/reminecraft/ReMinecraft.java
+++ src/com/sasha/reminecraft/ReMinecraft.java
@@ -45,7 +45,7 @@ public class ReMinecraft {
     /**
      * Current software version of Re:Minecraft
      */
-    public static final String VERSION = "1.0a";
+    public static final String VERSION = "1.0";
 
     public Logger logger = new Logger("RE:Minecraft " + VERSION);
     public Client minecraftClient = null;

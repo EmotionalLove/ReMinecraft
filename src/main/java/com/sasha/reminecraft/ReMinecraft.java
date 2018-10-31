023d25f586cc11a54fea6d0d36ed8e8a5a0ff14c
Sasha <sashwho@vivaldi.net>
2018-10-31 09:00:00 -0700

    Inc. Version

--- src/main/java/com/sasha/reminecraft/ReMinecraft.java
+++ src/main/java/com/sasha/reminecraft/ReMinecraft.java
@@ -48,7 +48,7 @@ public class ReMinecraft {
     /**
      * Current software version of Re:Minecraft
      */
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.1.1";
 
     public Logger logger = new Logger("RE:Minecraft " + VERSION);
     public Client minecraftClient = null;

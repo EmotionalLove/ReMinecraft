59ca695807883f6d4a087e679944fb1e125f54cf
Sasha <sashwho@vivaldi.net>
2018-11-13 14:47:34 -0800

    increment version

--- src/main/java/com/sasha/reminecraft/ReMinecraft.java
+++ src/main/java/com/sasha/reminecraft/ReMinecraft.java
@@ -49,7 +49,7 @@ public class ReMinecraft {
     /**
      * Current software version of Re:Minecraft
      */
-    public static final String VERSION = "1.1.6";
+    public static final String VERSION = "1.1.7";
 
     public Logger logger = new Logger("RE:Minecraft " + VERSION);
     public Client minecraftClient = null;

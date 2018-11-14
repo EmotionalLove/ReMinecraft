304fc770c0726641cdaf09f26e4b26cd825ba7e3
EmotionalLove <totsuka.sama@gmail.com>
2018-11-14 09:35:08 -0800

    Documentation

--- src/main/java/com/sasha/reminecraft/IReMinecraft.java
+++ src/main/java/com/sasha/reminecraft/IReMinecraft.java
@@ -7,15 +7,67 @@ import java.net.Proxy;
 
 public interface IReMinecraft {
 
+    /**
+     * Send a packet to all of the currently connected and set up children
+     *
+     * @param pck The packet to send
+     */
     void sendToChildren(Packet pck);
+
+    /**
+     * Load RE:Minecraft any any plugins
+     */
     void start(String[] args);
+
+    /**
+     * Authenticate with Mojang
+     *
+     * @param proxy the socks proxy to authenticate against
+     * @return A non-null object if successful
+     */
     AuthenticationService authenticate(Proxy proxy);
+
+    /**
+     * Update the session token inside of the config.
+     *
+     * @param token The new token
+     */
     void updateToken(String token);
+
+    /**
+     * Decide if any children are connected to RE:Minecraft
+     *
+     * @return bool
+     */
     boolean areChildrenConnected();
+
+    /**
+     * Register all of RE:Minecraft's commands
+     */
     void registerCommands();
+
+    /**
+     * Process a command sent in-game
+     *
+     * @param s A string beginning with the command prefix (backlash by default
+     * @return whether it was processed or not
+     */
     boolean processInGameCommand(String s);
+
+    /**
+     * Register all of the configurations and read them
+     */
     void registerConfigurations();
+
+    /**
+     * Stop RE:Minecraft
+     */
     void stop();
+
     void stopSoft();
+
+    /**
+     * Stop and restart RE:Minecraft
+     */
     void reLaunch();
 }

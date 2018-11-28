6151e76f10334ba0ece8e2794bcce84d1c140c0e
EmotionalLove <totsuka.sama@gmail.com>
2018-11-28 11:16:07 -0800

    Fix michu's weird code

--- src/main/java/com/sasha/reminecraft/api/event/ChatReceivedEvent.java
+++ src/main/java/com/sasha/reminecraft/api/event/ChatReceivedEvent.java
@@ -10,26 +10,31 @@ public class ChatReceivedEvent extends SimpleEvent {
     public String messageText;
     public long timeRecieved;
     public String messageAuthor;
-    
-    public ChatReceivedEvent(String messageText,String messageAuthor, long timeRecieved) {
+
+    public ChatReceivedEvent(String messageText, long timeRecieved) {
         this.messageText = messageText;
         this.timeRecieved = timeRecieved;
-        if(messageAuthor.startsWith("<"){
-            messageAuthor = event.messageText.replaceAll(".*\\<|\\>.*", ""); 
-        }else{
-            messageAuthor =null;
-        }
-        this.messageAuthor=messageAuthor
-    }     
+        if (messageText.startsWith("<")) messageAuthor = messageText.replaceAll(".*<.*?>.*", "");
+        else messageAuthor = null;
+    }
+
     public String getMessageText() {
         return messageText;
     }
-        this.messageAuthor = messageAuthor
+
     /**
      * In milliseconds
      */
     public long getTimeRecieved() {
         return timeRecieved;
+    }
+
+    public String getMessageAuthor() {
+        return messageAuthor;
+    }
+
+    public boolean hasMessageAuthor() {
+        return messageAuthor != null;
     }
 
     public void setMessageText(String messageText) {

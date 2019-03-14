8d5582118a35e9913d578edf3a8473fb83df007f
EmotionalLove <sashwho@vivaldi.net>
2019-03-14 09:13:23 -0700

    ip in childjoinevent

--- src/main/java/com/sasha/reminecraft/api/event/ChildJoinEvent.java
+++ src/main/java/com/sasha/reminecraft/api/event/ChildJoinEvent.java
@@ -3,13 +3,21 @@ package com.sasha.reminecraft.api.event;
 import com.github.steveice10.mc.auth.data.GameProfile;
 import com.sasha.eventsys.SimpleCancellableEvent;
 
+import java.net.SocketAddress;
+
 public class ChildJoinEvent extends SimpleCancellableEvent {
 
     private String cancelledKickMessage;
+    private SocketAddress childIpAddress;
     private GameProfile profile;
 
-    public ChildJoinEvent(GameProfile profile) {
+    public ChildJoinEvent(GameProfile profile, SocketAddress remoteAddress) {
         this.profile = profile;
+        this.childIpAddress = remoteAddress;
+    }
+
+    public SocketAddress getChildIpAddress() {
+        return childIpAddress;
     }
 
     public GameProfile getProfile() {
